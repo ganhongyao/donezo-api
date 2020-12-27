8c7ea20af96caf23b9d9b5e087a4d8b17af427a0
ganhongyao <ganhongyao@u.nus.edu>
2020-12-27 22:47:19 +0800

    Fixed issue that does not delete todos

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -10,7 +10,11 @@ module Api::V1
         render json: @todo
       end
 
-      
+      def update
+        @todo = Todo.find(params[:id])
+        @todo.update_attributes(todo_params)
+        render json: @todo
+      end
 
       def destroy
         @todo = Todo.find(params[:id])
